e49674f1d311e2ef5c66b2f51d62230df55d6f90
FoxySmile <142438460+FoxySmile@users.noreply.github.com>
2024-01-15 21:15:27 +0300

    Add files via upload

--- 15.01.24/DefaultClass.cpp
+++ 15.01.24/DefaultClass.cpp
@@ -2,22 +2,61 @@
 
 using namespace std;
 
-class Student {
+class Fraction {
 private:
-	string name;
-	int age;
+	int numerator;
+	int denominator;
+	int total = 0;
 public:
-	Student(string name, int age) {
-		this->age = age;
-		this->name = name;
+	Fraction(int numerator, int denominator) {
+		total = numerator / denominator;
+		this->denominator = denominator;
+		this->numerator = numerator % denominator;
 	}
 
 	void show() {
-		cout << "Name: " << name << ", age: " << age << endl;
+		if (total == 0) {
+			cout << numerator << "/" << denominator << endl;
+		}
+		if (numerator % denominator == 0) {
+			cout << total << endl;
+		}
+		else cout << total << " " << numerator << "/" << denominator << endl;
 	}
 };
 
+class Student {
+private:
+	string name;
+	int num;
+	double bal;
+public:
+	Student(string name, int num, int bal) {
+		this->name = name;
+		this->num = num;
+		this->bal = bal;
+	}
+	void show_all() {
+		cout << "Student: " << name << ", " << "grupa " << num << ", " << "bal " << bal << endl;
+	}
+
+	string getStatus() {
+		if (bal > 4.6) return "A";
+		if (bal > 3.6) return "B";
+		if (bal > 2.6) return "C";
+		else return "D";
+	}
+};
+
+
+
 int main() {
-	Student st1("Pol", 23);
-	st1.show();
+	/*Fraction f1(3, 6);
+	f1.show();*/
+	/*Student st1("Pol", 2, 4.5);
+	st1.show_all();
+	cout << "Status: " << st1.getStatus() << endl;*/
+
+	Fraction f2(12, 4);
+	f2.show();
 }
